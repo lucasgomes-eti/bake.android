422540006fd20f6f2ca17c594ab70524c473aad1
Lucas Gomes <lucas.gomes@nuvem.net>
2018-09-21 15:56:53 -0400

    Fix recipe detail navigate up

--- app/src/main/java/com/example/android/bake/ui/StepDetailActivity.java
+++ app/src/main/java/com/example/android/bake/ui/StepDetailActivity.java
@@ -26,15 +26,4 @@ public class StepDetailActivity extends AppCompatActivity {
                     .commit();
         }
     }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            // Respond to the action bar's Up/Home button
-            case android.R.id.home:
-                onBackPressed();
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
